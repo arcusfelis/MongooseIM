fde1b9d41dc2257126e24b2b795b4f6bac884576
Uvarov Michael <freeakk@gmail.com>
2013-07-27 20:54:49 +0400

    Using 2i with eleveldb.

--- apps/ejabberd/src/mod_mam_riak_arch.erl
+++ apps/ejabberd/src/mod_mam_riak_arch.erl
@@ -13,6 +13,25 @@
 -include_lib("ejabberd/include/jlib.hrl").
 -include_lib("exml/include/exml.hrl").
 
+message_bucket() ->
+    <<"mam_m">>.
+
+message_count_bucket() ->
+    <<"mam_c">>.
+
+message_count_by_bare_remote_jid_bucket() ->
+    <<"mam_cc">>.
+
+message_count_by_full_remote_jid_bucket() ->
+    <<"mam_ccc">>.
+
+user_id_bare_remote_jid_bucket() ->
+    "b".
+
+user_id_full_remote_jid_bucket() ->
+    "f".
+
+-define(MEASURE_TIME_TIMER, 1).
 -ifdef(MEASURE_TIME_TIMER).
 -define(MEASURE_TIME(Tag, Operation), measure_time((Tag), (fun() -> (Operation) end))). 
 measure_time(Tag, F) ->
@@ -39,9 +58,10 @@ measure_time(Tag, F) ->
 start(Host) ->
     application:start(riak_pool),
     F = fun(Conn) ->
-            update_bucket(Conn, <<"mam_messages">>, lww_props()),
-            update_bucket(Conn, <<"mam_hour_idx">>, [{allow_mult, true}]),
-            update_bucket(Conn, <<"mam_usr_msg_cnt">>, [{allow_mult, true}])
+            update_bucket(Conn, message_bucket(), lww_props()),
+            update_bucket(Conn, message_count_bucket(), [{allow_mult, true}]),
+            update_bucket(Conn, message_count_by_full_remote_jid_bucket(), [{allow_mult, true}]),
+            update_bucket(Conn, message_count_by_bare_remote_jid_bucket(), [{allow_mult, true}])
         end,
     with_connection(Host, F).
 
@@ -91,53 +111,61 @@ lookup_messages(UserJID=#jid{lserver=LServer},
 remove_user_from_db(LServer, LUser) ->
     ok.
 
+%% Reordered version.
+%% Allows sorting by `LServer'.
+%% The server host IS before the username.
+jid_to_opt_binary(LServer, #jid{lserver=LServer, luser=LUser, lresource= <<>>}) ->
+    %% Both clients are on the same server.
+    %% Remote JID is bare.
+    %% We cannot use `user_id_to_binary/2', because the remote user can be
+    %% deleted from the DB.
+    <<$@, LUser/binary>>;
+jid_to_opt_binary(LServer, #jid{lserver=LServer, luser=LUser, lresource=LResource}) ->
+    %% Both clients are on the same server.
+    <<$@, LUser/binary, $/, LResource/binary>>;
+jid_to_opt_binary(_, #jid{lserver=LServer, luser=LUser, lresource=LResource}) ->
+    <<LServer/binary, $@, LUser/binary, $/, LResource/binary>>.
 
-archive_message(Id, Dir, _LocJID=#jid{luser=LocLUser, lserver=LocLServer},
+
+archive_message(MessID, Dir, _LocJID=#jid{luser=LocLUser, lserver=LocLServer},
                 RemJID=#jid{lresource=RemLResource}, SrcJID, Packet) ->
     UserID = mod_mam_cache:user_id(LocLServer, LocLUser),
-    Data = term_to_binary(Packet, [compressed]),
-    BBareRemJID = jlib:jid_to_binary(
-            jlib:jid_tolower(jlib:jid_remove_resource(RemJID))),
-    BRemJID = jlib:jid_to_binary(jlib:jid_tolower(RemJID)),
-    BID = id_to_binary(Id),
     BUserID = user_id_to_binary(UserID),
-    BHourID = hour_id(Id, UserID),
-    BRemJIDHourID = remote_jid_hour_id(BHourID, BRemJID),
-    BBareRemJIDHourID = remote_jid_hour_id(BHourID, BBareRemJID),
-    BShortMessID = short_message_id(Id),
-    BUserIDRemJID = remote_jid_message_id(Id, BRemJID),
-    BUserIDBareRemJID = remote_jid_message_id(Id, BBareRemJID),
+    BMessID = mess_id_to_binary(MessID),
+    BBareRemJID = jid_to_opt_binary(LocLServer, jlib:jid_remove_resource(RemJID)),
+    BFullRemJID = jid_to_opt_binary(LocLServer, RemJID),
+    BUserIDFullRemJIDMessID = user_remote_jid_message_id(BUserID, BMessID, BFullRemJID),
+    BUserIDBareRemJIDMessID = user_remote_jid_message_id(BUserID, BMessID, BBareRemJID),
+    Key = message_key(BUserID, BMessID),
+    Data = term_to_binary(Packet, [compressed]),
     F = fun(Conn) ->
-        %% Request a Riak client's ID from the server.
-        {ok, ClientID} = ?MEASURE_TIME(get_client_id,
-            riakc_pb_socket:get_client_id(Conn)),
         %% Save message body.
-        Obj = riakc_obj:new(<<"mam_messages">>, BID, Data),
-        ok = ?MEASURE_TIME(put_message, riakc_pb_socket:put(Conn, Obj)),
-        %% Increment total message count in archive.
-        part_counter_incr(Conn, ClientID, BUserID, BHourID, 1),
-        part_counter_incr(Conn, ClientID, BUserIDRemJID, BHourID, 1),
-        part_counter_incr(Conn, ClientID, BUserIDBareRemJID, BHourID, 1),
-        %% Write offset into index.
-        update_hour_index(Conn, BHourID, BShortMessID),
-        update_hour_index(Conn, BRemJIDHourID, BShortMessID),
-        update_hour_index(Conn, BBareRemJIDHourID, BShortMessID),
+        Obj = riakc_obj:new(message_bucket(), Key, Data),
+        Obj1 = set_index(Obj, BUserIDFullRemJIDMessID, BUserIDBareRemJIDMessID),
+        %% TODO: handle `{error, Reason}'. Should we reinsert again later?
+        ok = ?MEASURE_TIME(put_message, riakc_pb_socket:put(Conn, Obj1)),
         ok
     end,
     with_connection(LocLServer, F),
     ok.
+
+set_index(Obj, BUserIDFullRemJIDMessID, BUserIDBareRemJIDMessID) ->
+    MD = riakc_obj:get_metadata(Obj),
+    Is = [{{binary_index, user_id_full_remote_jid_bucket()}, [BUserIDFullRemJIDMessID]},
+          {{binary_index, user_id_bare_remote_jid_bucket()}, [BUserIDBareRemJIDMessID]}],
+    MD1 = riakc_obj:set_secondary_index(MD, Is),
+    riakc_obj:update_metadata(Obj, MD1).
 
 
 with_connection(LocLServer, F) ->
     ?MEASURE_TIME(with_connection, 
         riak_pool:with_connection(mam_cluster, F)).
 
-
-id_to_binary(Id) ->
-    <<Id:64/big>>.
-
 user_id_to_binary(UserID) ->
     <<UserID:64/big>>.
+
+mess_id_to_binary(MessID) ->
+    <<MessID:64/big>>.
 
 hour_id(MessID, UserID) ->
     {Microseconds, _} = mod_mam_utils:decode_compact_uuid(MessID),
@@ -180,7 +208,14 @@ part_counter_incr(Conn, ClientID, Key, BHourID, N)
     case ?MEASURE_TIME(part_counter_incr_get,
                        riakc_pb_socket:get(Conn, <<"mam_usr_msg_cnt">>, Key)) of
         {ok, Obj} ->
-            MergedValue = part_pb_counter:merge(riakc_obj:get_values(Obj)),
+            Values = riakc_obj:get_values(Obj),
+            case Values of
+                [_,_|_] -> %% length(Values) > 1
+                    ?DEBUG("Merge ~p siblings.", [length(Values)]);
+                _ ->
+                    ok
+            end,
+            MergedValue = part_pb_counter:merge(Values),
             Value = part_pb_counter:increment(BHourID, N, ClientID, MergedValue),
             Obj2 = riakc_obj:update_value(Obj, Value),
             Obj3 = fix_metadata(Obj2),
@@ -207,15 +242,27 @@ fix_metadata(Obj) ->
 merge_metadatas([Meta|_]) ->
     Meta.
 
-merge_short_message_ids(GroupedIDS) ->
-    IDS = lists:usort([ID || IDS <- GroupedIDS, <<ID:40>> <= IDS]),
-    << <<ID:40>> || ID <- IDS>>.
+merge_short_message_ids(GroupedIDs) ->
+    case GroupedIDs of
+        [_,_,_|_] -> %% length(GroupedIDs) > 2
+            ?DEBUG("Merge ~p siblings.", [length(GroupedIDs)-1]);
+        _ ->
+            ok
+    end,
+    SortedLists = ([[ID || <<ID:40>> <= IDs] || IDs <- GroupedIDs]),
+    IDs = lists:merge(SortedLists),
+    << <<ID:40>> || ID <- IDs>>.
 
 remote_jid_hour_id(BHourID, BJID) ->
     <<BHourID/binary, BJID/binary>>.
 
-remote_jid_message_id(Id, BJID) ->
-    <<Id:64/big, BJID/binary>>.
+message_key(BUserID, BMessID)
+    when is_binary(BUserID), is_binary(BMessID) ->
+    <<BUserID/binary, BMessID/binary>>.
+
+user_remote_jid_message_id(BUserID, BMessID, BJID)
+    when is_binary(BUserID), is_binary(BMessID), is_binary(BJID) ->
+    <<BUserID/binary, BJID/binary, BMessID/binary>>.
 
 
 %% @doc Ensure, that `UpdateProps' are set in required position.
