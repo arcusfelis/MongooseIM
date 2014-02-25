5853bb8313b776bfca4086853aabcd41b0d0b205
Michael Uvarov <michael.uvarov@erlang-solutions.com>
2014-02-25 11:41:03 +0100

    Add multi fetch support

--- apps/ejabberd/src/mod_offline_kafka.erl
+++ apps/ejabberd/src/mod_offline_kafka.erl
@@ -34,15 +34,30 @@
          remove_old_messages/2,
          remove_user/2]).
 
+%% Internal exports
+-export([start_link/3]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
 -include("ejabberd.hrl").
 -include("jlib.hrl").
 -include("mod_offline.hrl").
+
+-record(state, {
+    flush_interval=500,
+    max_packet_size=30,
+    host,
+    conn,
+    acc=[],
+    flush_interval_tref}).
 
 -define(OFFLINE_TABLE_LOCK_THRESHOLD, 1000).
 
 -record(offline_msg_offset, {us, offset}).
 
-init(_Host, Opts) ->
+init(Host, Opts) ->
     %% Servers will not be stopped with module.
     Servers = gen_mod:get_opt(kafka_servers, Opts, [{0, '127.0.0.1', 9092}]),
     kafka_server_sup:start_link(Servers),
@@ -50,7 +65,57 @@ init(_Host, Opts) ->
             [{disc_copies, [node()]},
              {type, set},
              {attributes, record_info(fields, offline_msg_offset)}]),
+    start_workers(Host),
     ok.
+
+%% Supervision
+%% ------------------------------------------------------------------
+
+pool_size(_Host) ->
+    4.
+
+start_workers(Host) ->
+    [start_worker(Host, N)
+    || N <- lists:seq(1, pool_size(Host))].
+
+stop_workers(Host) ->
+    [stop_worker(Host, N)
+    || N <- lists:seq(1, pool_size(Host))].
+    
+start_worker(Host, N) ->
+    Proc = srv_name(Host, N),
+    ChildSpec =
+    {Proc,
+     {?MODULE, start_link, [Proc, Host, 0]},
+     permanent,
+     5000,
+     worker,
+     [?MODULE]},
+    supervisor:start_child(ejabberd_sup, ChildSpec),
+    ok.
+
+stop_worker(Host, N) ->
+    Proc = srv_name(Host, N),
+    supervisor:terminate_child(ejabberd_sup, Proc),
+    supervisor:delete_child(ejabberd_sup, Proc).
+
+start_link(Name, Host, Broker) ->
+    gen_server:start_link({local, Name}, ?MODULE, [Host, Broker], []).
+
+choose_server(Host, LUser) ->
+    Size = pool_size(Host),
+    %% N = 1 .. Size
+    N = erlang:phash2(LUser, Size) + 1,
+    srv_name(Host, N).
+
+srv_name(Host, N) when is_binary(Host), is_integer(N) ->
+    SHost = binary_to_list(Host),
+    SN = integer_to_list(N),
+    list_to_atom("mod_offline_" ++ SHost ++ "_" ++ SN).
+
+
+%% API
+%% ------------------------------------------------------------------
 
 pop_messages(LUser, LServer) ->
     US = {LUser, LServer},
@@ -59,7 +124,9 @@ pop_messages(LUser, LServer) ->
     Broker  = 0,
     Part    = 0,
     Offset = get_offset(US),
-    case kafka_simple_api:fetch(Broker, Topic, Part, Offset) of
+    Server = choose_server(LServer, LUser),
+    MaxSize = 65535,
+    case fetch_request(Server, Topic, Part, Offset, MaxSize) of
         {ok, []} ->
             {ok, []};
         {ok, {Bins, Size}} ->
@@ -70,6 +137,9 @@ pop_messages(LUser, LServer) ->
                 [Topic, Offset, Reason]),
             {error, kafka_error}
     end.
+
+fetch_request(Server, Topic, Part, Offset, MaxSize) ->
+    gen_server:call(Server, {fetch_request, {Topic, Part, Offset, MaxSize}}).
 
 get_offset(US) ->
     case mnesia:dirty_read(offline_msg_offset, US) of
@@ -141,3 +211,95 @@ remove_expired_messages(_LServer) ->
 
 remove_old_messages(_LServer, _Days) ->
     ok.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+run_flush(State=#state{acc=[]}) ->
+    State;
+run_flush(State=#state{conn=Conn, flush_interval_tref=TRef, acc=Acc}) ->
+    ?DEBUG("Multifetch ~p topics.", [length(Acc)]),
+    {Clients, TopicPartitionOffsets} = lists:unzip(Acc),
+    TRef =/= undefined andalso erlang:cancel_timer(TRef),
+    {ok, Messages} = kafka_simple_api:multi_fetch(Conn, TopicPartitionOffsets),
+    [gen_server:reply(From, {ok, Message})
+     || {From, Message} <- lists:zip(Clients, Messages)],
+    State#state{acc=[], flush_interval_tref=undefined}.
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([Host, Broker]) ->
+    {ok, #state{host=Host, conn=Broker}}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call({fetch_request, TopicPartitionOffset}, From,
+            State=#state{acc=Acc, flush_interval_tref=TRef, flush_interval=Int,
+                         max_packet_size=Max}) ->
+    TRef2 = case {Acc, TRef} of
+            {[], undefined} -> erlang:send_after(Int, self(), flush);
+            {_, _} -> TRef
+            end,
+    Req = {From, TopicPartitionOffset},
+    State2 = State#state{acc=[Req|Acc], flush_interval_tref=TRef2},
+    case length(Acc) + 1 >= Max of
+        true -> {noreply, run_flush(State2)};
+        false -> {noreply, State2}
+    end.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+
+handle_cast(Msg, State) ->
+    ?WARNING_MSG("Strange message ~p.", [Msg]),
+    {noreply, State}.
+
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+
+handle_info(flush, State) ->
+    {noreply, run_flush(State)}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
