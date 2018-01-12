d09dd9c6fbd7cf6e26de960976e33edf2c2ab7cc
Michael Uvarov <arcusfelis@gmail.com>
2018-01-12 12:35:03 +0100

    Hardcode database password in test scripts

--- tools/travis-build-tests.sh
+++ tools/travis-build-tests.sh
@@ -2,8 +2,5 @@
 
 source tools/travis-common-vars.sh
 
-TRAVIS_DB_PASSWORD=$(cat /tmp/travis_db_password)
-${TOOLS}/set-odbc-password test ${TRAVIS_DB_PASSWORD}
-
 cd ${BASE}/test.disabled/ejabberd_tests && make prepare
 
