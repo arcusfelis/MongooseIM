489a8adfe1754788c87aa77790089210f7612ba5
Michael Uvarov <arcusfelis@gmail.com>
2018-03-05 16:56:47 +0100

    Make log_parallel_story_stanza_drop configurable

--- test.disabled/ejabberd_tests/tests/mam_SUITE.erl
+++ test.disabled/ejabberd_tests/tests/mam_SUITE.erl
@@ -2967,18 +2967,20 @@ should_pass_parallel_stanza_jid(Jid, StoryPidBin) ->
             true
     end.
 
-log_parallel_story_stanza_drop(_Pass = true, Stanza, From, To, StoryPidBin) ->
-    ok;
-log_parallel_story_stanza_drop(_Pass = false, Stanza, From, To, StoryPidBin) ->
+log_parallel_story_stanza_drop(_Pass = false, _LogEnabled = true,
+                               Stanza, From, To, StoryPidBin) ->
     ct:log(default, 50,
            "drop stanza from_jid=~ts to_jid=~ts my_story_pid=~ts~nstanza=~ts",
            [From, To, StoryPidBin, exml:to_binary(Stanza)],
            [esc_chars]),
+    ok;
+log_parallel_story_stanza_drop(_Pass, _LogEnabled,
+                               _Stanza, _From, _To, _StoryPidBin) ->
     ok.
 
 initial_activity(Config) ->
     %% Can be enabled in test.config
-    LogParallelStanzaDrop = proplists:get_value(log_parallel_story_stanza_drop, Config, false),
+    LogDropEnabled = proplists:get_value(log_parallel_story_stanza_drop, Config, false),
     StoryPidBin = list_to_binary(pid_to_list(self())),
     fun(Client) ->
         Pred = fun
@@ -2989,7 +2991,8 @@ initial_activity(Config) ->
                         %% Useful for debugging
                         %% Pretty heavy for real tests
                         %% Enable, when you see problems with parallel_story cases
-                        log_parallel_story_stanza_drop(Pass, Stanza, From, To, StoryPidBin),
+                        log_parallel_story_stanza_drop(Pass, LogDropEnabled,
+                                                       Stanza, From, To, StoryPidBin),
                         Pass;
                    (_) -> true %% pass xmlstreamend
                end,
