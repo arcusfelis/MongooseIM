e1de842a3c70177ab08ae23dc1b810373da847fc
Mikhail Uvarov <arcusfelis@gmail.com>
2019-06-13 15:58:46 +0200

    Add mongooseimctl stop_forced

--- tools/stop-nodes.sh
+++ tools/stop-nodes.sh
@@ -17,7 +17,7 @@ source tools/test_runner/helpers.sh
 # Fails if release for the node is not compiled
 stop_node() {
   echo -n "${1} stop: "
-  ${BASE}/_build/${1}/rel/mongooseim/bin/mongooseimctl stop && echo ok || echo failed
+  ${BASE}/_build/${1}/rel/mongooseim/bin/mongooseimctl stop_forced && echo ok
 }
 
 async_helper() {
