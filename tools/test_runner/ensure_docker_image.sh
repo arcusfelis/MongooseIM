f26c4d61d1a0eb850cdd3658b30959df47117301
Mikhail Uvarov <arcusfelis@gmail.com>
2019-06-20 09:28:11 +0200

    Limit time of docker build and retry

--- tools/test_runner/ensure_docker_image.sh
+++ tools/test_runner/ensure_docker_image.sh
@@ -8,6 +8,28 @@ function do_bootstap
     locale-gen en_US.UTF-8
 }
 
+function retry_on_timeout 
+{
+    local TIMEOUT=$1
+    local RETRIES=$2
+    shift 2
+
+    if hash timeout ; then
+        if timeout "$TIMEOUT" "$@" ; then
+            echo "retry_on_timeout: success"
+        elif "$RETRIES" = "0" ; then
+            echo "retry_on_timeout: failed"
+            exit 1
+        else
+            echo "retry_on_timeout: retry"
+            retry_on_timeout "$TIMEOUT" "$(($RETRIES - 1))" "$@"
+        fi
+    else
+        echo "timeout command not found, try without it"
+        "$@"
+    fi
+}
+
 case "$1" in
     ensure)
         # Create and bootstrap image
@@ -18,7 +40,7 @@ case "$1" in
         cp ./tools/test_runner/ensure_docker_image.sh _build/.test_runner/docker_build/
         cd _build/.test_runner/docker_build
 
-        docker build -t "$IMAGE" -f - . <<EOF
+        retry_on_timeout 120s 5 docker build -t "$IMAGE" -f - . <<EOF
 FROM "erlang:$ERLANG_VERSION"
 ADD ensure_docker_image.sh ensure_docker_image.sh
 RUN ./ensure_docker_image.sh bootstap
