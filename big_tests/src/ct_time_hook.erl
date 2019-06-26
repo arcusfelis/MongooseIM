de348ba566b3d572340714e17214928e04601821
Mikhail Uvarov <arcusfelis@gmail.com>
2019-06-26 20:08:46 +0200

    Handle parallel testcases in ct_time_hook

--- big_tests/src/ct_time_hook.erl
+++ big_tests/src/ct_time_hook.erl
@@ -26,7 +26,7 @@
 
  -export([terminate/1]).
 
- -record(state, { file_handle, total, suite_total, ts, tcs, data }).
+ -record(state, { file_handle, ts, suite_ts, events }).
 
  %% Return a unique id for this CTH.
  id(Opts) ->
@@ -36,15 +36,15 @@
  %% any common state. 
  init(Id, Opts) ->
      {ok,D} = file:open(Id,[write]),
-     {ok, #state{ file_handle = D, total = 0, data = [] }}.
+     {ok, #state{ file_handle = D }}.
 
  %% Called before init_per_suite is called.
  pre_init_per_suite(Suite,Config,State) ->
-     {Config, State#state{ suite_total = 0, tcs = [] }}.
+     {Config, State#state{ events = [] }}.
 
  %% Called after init_per_suite.
  post_init_per_suite(Suite,Config,Return,State) ->
-     {Return, State}.
+     {Return, State#state{ suite_ts = os:timestamp() }}.
 
  %% Called before end_per_suite.
  pre_end_per_suite(Suite,Config,State) ->
@@ -52,9 +52,10 @@
 
  %% Called after end_per_suite.
  post_end_per_suite(Suite,Config,Return,State) ->
-     Data = {suites, Suite, State#state.suite_total, lists:reverse(State#state.tcs)},
-     {Return, State#state{ data = [Data | State#state.data] ,
-                           total = State#state.total + State#state.suite_total } }.
+     Info = suite_info(Suite, State),
+     Data = [Info | lists:reverse(State#state.events)],
+     [io:format(State#state.file_handle, "~p.~n", [X]) || X <- Data],
+     {Return, State#state{ events = [], suite_ts = undefined } }.
 
  %% Called before each init_per_group.
  pre_init_per_group(Suite,Group,Config,State) ->
@@ -74,7 +75,7 @@
 
  %% Called before each init_per_testcase.
  pre_init_per_testcase(Suite,TC,Config,State) ->
-     {Config, State#state{ ts = os:timestamp(), total = State#state.suite_total + 1 } }.
+     {Config, State#state{ ts = os:timestamp() } }.
 
  %% Called after each init_per_testcase (immediately before the test case).
  post_init_per_testcase(Suite,TC,Config,Return,State) ->
@@ -85,11 +86,9 @@ pre_end_per_testcase(Suite,TC,Config,State) ->
      {Config, State}.
 
  %% Called after each end_per_testcase.
- post_end_per_testcase(Suite,TC,Config, ok, State = #state{ ts = {_,_,_}}) ->
-     TCInfo = {testcase, Suite, TC, State#state.ts, timer:now_diff(os:timestamp(), State#state.ts)},
-     {Return, State#state{ ts = undefined, tcs = [TCInfo | State#state.tcs] } };
- post_end_per_testcase(Suite,TC,Config,Return,State) ->
-     {Return, State#state{ ts = undefined}}.
+ post_end_per_testcase(Suite,TC,Config, Return, State) ->
+     TCInfo = testcase_info(Suite, TC, State),
+     {Return, State#state{events = [TCInfo | State#state.events] } }.
 
  %% Called after post_init_per_suite, post_end_per_suite, post_init_per_group,
  %% post_end_per_group and post_end_per_testcase if the suite, group or test case failed.
@@ -103,7 +102,15 @@ pre_end_per_testcase(Suite,TC,Config,State) ->
 
  %% Called when the scope of the CTH is done
  terminate(State) ->
-     io:format(State#state.file_handle, "~p.~n",
-                [{test_run, State#state.total, State#state.data}]),
      file:close(State#state.file_handle),
      ok.
+
+suite_info(Suite, State = #state{suite_ts = {_,_,_}}) ->
+     {suite, Suite, State#state.suite_ts, timer:now_diff(os:timestamp(), State#state.suite_ts)};
+suite_info(Suite, _State) ->
+     {suite, Suite, {0,0,0}, 0}.
+
+testcase_info(Suite, TC, State = #state{ts = {_,_,_}}) ->
+     {testcase, Suite, TC, State#state.ts, timer:now_diff(os:timestamp(), State#state.ts)};
+testcase_info(Suite, TC, _State) ->
+     {testcase, Suite, TC, {0,0,0}, 0}.
