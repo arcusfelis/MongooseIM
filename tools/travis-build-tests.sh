50fe0dc9948ffefdbcf9430a9d1cfc0b70936ab6
Mikhail Uvarov <arcusfelis@gmail.com>
2019-06-27 16:13:58 +0200

    Fail travis-build-tests.sh on error

--- tools/travis-build-tests.sh
+++ tools/travis-build-tests.sh
@@ -5,6 +5,8 @@
 
 source tools/travis-common-vars.sh
 
+set -e
+
 # rebar doesn't like Unicode in commit messages #1674
 # So, we unset commit message env variable
 # TODO: Remove it when we switch to rebar3 for big tests.
