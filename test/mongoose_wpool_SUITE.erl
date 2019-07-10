c87ad1e994fdd68c753baadfab1261c128b79945
Mikhail Uvarov <arcusfelis@gmail.com>
2019-07-10 17:54:32 +0200

    Fix undefined pid error in dead_pool_is_restarted

--- test/mongoose_wpool_SUITE.erl
+++ test/mongoose_wpool_SUITE.erl
@@ -187,9 +187,9 @@ dead_pool_is_restarted(_C) ->
     {PoolName, KillingSwitch} = start_killable_pool(Size, kill_and_restart),
     %% set the switch to kill every new worker
     set_killing_switch(KillingSwitch, true),
+    Pids = [whereis(wpool_pool:worker_name(PoolName, I)) || I <- lists:seq(1, Size)],
     %% kill existing workers so they will be restarted
-    [erlang:exit(whereis(wpool_pool:worker_name(PoolName, I)), kill) ||
-     I <- lists:seq(1, Size)],
+    [erlang:exit(Pid, kill) || Pid <- Pids, Pid =/= undefined],
 
     wait_until_pool_is_dead(PoolName),
     %% set the switch to stop killing workers
@@ -259,13 +259,7 @@ start_killable_pool(Size, Tag) ->
               []}],
     [{ok, _Pid}] = mongoose_wpool:start_configured_pools(Pools),
     PoolName = mongoose_wpool:make_pool_name(generic, global, Tag),
-    assert_all_workers_are_up(PoolName, Size),
     {PoolName, KillingSwitch}.
-
-assert_all_workers_are_up(PoolName, Size) ->
-    WorkerNames = [wpool_pool:worker_name(PoolName, I) || I <- lists:seq(1, Size)],
-    NotStartedFun = fun() -> [W || W <- WorkerNames, whereis(W) =:= undefined] end,
-    async_helper:wait_until(NotStartedFun, []).
 
 kill_worker_fun(KillingSwitch) ->
     fun(AWorkerName) ->
