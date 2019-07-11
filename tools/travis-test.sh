6bdedd051c7e35a37ebd5faac97d47cc84463861
Mikhail Uvarov <arcusfelis@gmail.com>
2019-07-11 22:06:00 +0200

    Properly set BIG_STATUS code on restart

--- tools/travis-test.sh
+++ tools/travis-test.sh
@@ -184,6 +184,7 @@ run_tests() {
       ./tools/test_runner/selected-tests-to-test-spec.sh $(cat /tmp/ct_summary)
       export TESTSPEC=auto_big_tests.spec
 
+     BIG_STATUS=0
       # Disable cover for rerun
       COVER_ENABLED=false run_test_preset || BIG_STATUS=$?
   fi
