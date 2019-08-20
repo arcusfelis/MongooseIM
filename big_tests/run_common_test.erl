d59bbf7d982fc123fbac6154008bd60fb53bd0ad
Mikhail Uvarov <arcusfelis@gmail.com>
2019-08-20 12:59:50 +0200

    Move code into mim_cover_report from run_common_tests

--- big_tests/run_common_test.erl
+++ big_tests/run_common_test.erl
@@ -23,9 +23,6 @@
 
 -export([ct_run_test/1]).
 
--define(CT_DIR, filename:join([".", "tests"])).
--define(CT_REPORT, filename:join([".", "ct_report"])).
-
 %% DEBUG: compile time settings
 -define(PRINT_ERRORS, false).
 -define(PRINT_STATS, false).
@@ -378,7 +375,7 @@ analyze(Test, CoverOpts, Nodes) ->
 		end),
     case os:getenv("TRAVIS_JOB_ID") of
         false ->
-            make_html(modules_to_analyze(CoverOpts));
+            mim_cover_report:make_html(modules_to_analyze(CoverOpts));
         _ ->
             ok
     end,
@@ -391,41 +388,6 @@ analyze(Test, CoverOpts, Nodes) ->
                         cover:stop([node()|Nodes])
                     end)
     end.
-
-make_html(Modules) ->
-    {ok, Root} = file:get_cwd(),
-    SortScript = Root ++ "/priv/sorttable.js",
-    os:cmd("cp " ++ SortScript ++ " " ++ ?CT_REPORT),
-    FilePath = case file:read_file(?CT_REPORT++"/index.html") of
-        {ok, IndexFileData} ->
-            R = re:replace(IndexFileData, "<a href=\"all_runs.html\">ALL RUNS</a>", "& <a href=\"cover.html\" style=\"margin-right:5px\">COVER</a>"),
-            file:write_file(?CT_REPORT++"/index.html", R),
-            ?CT_REPORT++"/cover.html";
-        _ -> skip
-    end,
-    CoverageDir = filename:dirname(FilePath)++"/coverage",
-    file:make_dir(CoverageDir),
-    {ok, File} = file:open(FilePath, [write]),
-    file:write(File, get_cover_header()),
-    Fun = fun(Module, {CAcc, NCAcc}) ->
-                  FileName = lists:flatten(io_lib:format("~s.COVER.html",[Module])),
-
-                  %% We assume that import_code_paths/1 was called earlier
-                  case cover:analyse(Module, module) of
-                      {ok, {Module, {C, NC}}} ->
-                          file:write(File, row(atom_to_list(Module), C, NC, percent(C,NC),"coverage/"++FileName)),
-                          FilePathC = filename:join([CoverageDir, FileName]),
-                          catch cover:analyse_to_file(Module, FilePathC, [html]),
-                          {CAcc + C, NCAcc + NC};
-                      Reason ->
-                          error_logger:error_msg("issue=cover_analyse_failed module=~p reason=~p",
-                                                 [Module, Reason]),
-                          {CAcc, NCAcc}
-                  end
-          end,
-    {CSum, NCSum} = lists:foldl(Fun, {0, 0}, Modules),
-    file:write(File, row("Summary", CSum, NCSum, percent(CSum, NCSum), "#")),
-    file:close(File).
 
 get_hosts_to_enable_preset(Test) ->
     Hosts = get_all_hosts(Test),
@@ -440,28 +402,6 @@ get_all_hosts(Test) ->
 
 get_mongoose_nodes(Test) ->
     [ host_node(H) || H <- get_all_hosts(Test), is_test_host_enabled(host_name(H)) ].
-
-percent(0, _) -> 0;
-percent(C, NC) when C /= 0; NC /= 0 -> round(C / (NC+C) * 100);
-percent(_, _)                       -> 100.
-
-row(Row, C, NC, Percent, Path) ->
-    [
-        "<tr>",
-        "<td><a href='", Path, "'>", Row, "</a></td>",
-        "<td>", integer_to_list(Percent), "%</td>",
-        "<td>", integer_to_list(C), "</td>",
-        "<td>", integer_to_list(NC), "</td>",
-        "<td>", integer_to_list(C+NC), "</td>",
-        "</tr>\n"
-    ].
-
-get_cover_header() ->
-    "<html>\n<head></head>\n<body bgcolor=\"white\" text=\"black\" link=\"blue\" vlink=\"purple\" alink=\"red\">\n"
-    "<head><script src='sorttable.js'></script></head>"
-    "<h1>Coverage for application 'MongooseIM'</h1>\n"
-    "<table class='sortable' border=3 cellpadding=5>\n"
-    "<tr><th>Module</th><th>Covered (%)</th><th>Covered (Lines)</th><th>Not covered (Lines)</th><th>Total (Lines)</th></tr>".
 
 bool_or_module_list("true") ->
     true;
