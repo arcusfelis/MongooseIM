b50829be3e4508d8bf928d16ffbc4c5b2e2d55e9
Mikhail Uvarov <arcusfelis@gmail.com>
2019-08-23 11:24:15 +0200

    Setup mysql db container per job

--- big_tests/src/mim_ct_db.erl
+++ big_tests/src/mim_ct_db.erl
@@ -25,7 +25,16 @@ init_job_db(mssql, TestConfig = #{prefix := Prefix}) ->
     DbName = Prefix ++ "_mim_db",
     setup_mssql_database(DbName),
     TestConfig#{mssql_database => DbName};
+init_job_db(mysql, TestConfig = #{prefix := Prefix, hosts := Hosts}) ->
+    %% Usually just one port at this point, already rewritten
+    %% But can be no ports (then do nothing)
+    %% Or more than one ???
+    MysqlPorts = lists:delete(none, lists:usort([proplists:get_value(mysql_port, Host, none) || {_HostId, Host} <- Hosts])),
+    io:format("MysqlPorts ~p~n", [MysqlPorts]),
+    [setup_mysql_container(DbPort, Prefix ++ "_mim_db_" ++ integer_to_list(DbPort)) || DbPort <- MysqlPorts],
+    TestConfig;
 init_job_db(Db, TestConfig) ->
+    io:format("init_job_db: Do nothing for db ~p~n", [Db]),
     TestConfig.
 
 get_databases(MasterConfig, JobConfigs) ->
@@ -51,6 +60,8 @@ preset_to_databases(Preset, JobConfig = #{ejabberd_presets := Presets}) ->
     PresetOpts =  proplists:get_value(Preset, Presets, []),
     proplists:get_value(dbs, PresetOpts, []).
 
+init_db(mysql) ->
+    {done, 0, "skip"};
 init_db(DbType) ->
     RepoDir = path_helper:repo_dir([]),
     mim_ct_sh:run([filename:join([RepoDir, "tools", "travis-setup-db.sh"])], #{env => #{"DB" => atom_to_list(DbType), "DB_PREFIX" => "mim-ct1"}, cwd => RepoDir}).
@@ -58,3 +69,10 @@ init_db(DbType) ->
 setup_mssql_database(DbName) ->
     RepoDir = path_helper:repo_dir([]),
     mim_ct_sh:run([filename:join([RepoDir, "tools", "setup-mssql-database.sh"])], #{env => #{"DB_NAME" => DbName, "DB_PREFIX" => "mim-ct1"}, cwd => RepoDir}).
+
+setup_mysql_container(DbPort, Prefix) ->
+    RepoDir = path_helper:repo_dir([]),
+    Envs =#{env => #{"DB" => "mysql", "MYSQL_PORT" => integer_to_list(DbPort), "DB_PREFIX" => "mim-ct1-" ++ Prefix}, cwd => RepoDir},
+    {done, _, Result} = mim_ct_sh:run([filename:join([RepoDir, "tools", "travis-setup-db.sh"])], Envs),
+    io:format("Setup mysql container ~p returns ~ts~n", [DbPort, Result]),
+    ok.
