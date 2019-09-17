56d12bba7fc0bc3e683d47102bed0e4b1105e190
Mikhail Uvarov <arcusfelis@gmail.com>
2019-09-17 12:44:44 +0200

    Better log printing formatting

--- big_tests/src/mim_node.erl
+++ big_tests/src/mim_node.erl
@@ -87,7 +87,7 @@ print_log_file(Node, LogFile) ->
                     catch io:format("Failed to read file ~p~n", [Error])
             end
         end,
-    mim_ct_helper:travis_fold("Log", "Log " ++ filename:basename(LogFile) ++ " from " ++ atom_to_list(Node), F).
+    mim_ct_helper:travis_fold("Log", "Log from " ++ atom_to_list(Node) ++ " " ++ filename:basename(LogFile), F).
 
 clean_logs(NodeConfig = #{build_dir := BuildDir}) ->
     LogDir = filename:join(BuildDir, "rel/mongooseim/log"),
