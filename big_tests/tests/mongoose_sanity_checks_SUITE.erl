5ae0cdbe83b04339ab59fe99308f8b70e98f5543
Mikhail Uvarov <arcusfelis@gmail.com>
2019-09-17 15:03:19 +0200

    Wait for mongooseim starting accepting connections in tests

--- big_tests/tests/mongoose_sanity_checks_SUITE.erl
+++ big_tests/tests/mongoose_sanity_checks_SUITE.erl
@@ -3,8 +3,33 @@
 -compile(export_all).
 
 all() ->
-    [is_mongooseim].
+    [is_mongooseim,
+     {group, healthcheck}].
+
+groups() ->
+    %% Wait for mongooseim starting accepting connections
+    [{healthcheck,  [{repeat_until_all_ok, 30}], healthcheck_cases()}].
+
+healthcheck_cases() ->
+    [alice_can_connect].
+
+
+init_per_suite(Config) ->
+    escalus:init_per_suite(Config).
+
+end_per_suite(Config) ->
+    escalus:end_per_suite(Config).
+
+init_per_testcase(CaseName, Config) ->
+    escalus:init_per_testcase(CaseName, Config).
+
+end_per_testcase(CaseName, Config) ->
+    escalus:end_per_testcase(CaseName, Config).
 
 
 is_mongooseim(Config) ->
     mongooseim = escalus_server:name(Config).
+
+alice_can_connect(Config) ->
+    AliceSpec = escalus_fresh:create_fresh_user(Config, alice),
+    {ok, _, _} = escalus_connection:start(AliceSpec).
